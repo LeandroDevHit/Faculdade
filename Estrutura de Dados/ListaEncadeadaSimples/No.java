50cefe193547dc4111a4e775c8cfdb5717b9b23a
LoneHitori <leandrohitorii@gmail.com>
2024-10-02 14:36:58 -0300

    Update No.java

--- Estrutura de Dados/ListaEncadeadaSimples/No.java
+++ Estrutura de Dados/ListaEncadeadaSimples/No.java
@@ -1,1 +1,29 @@
+package listencadeada;
 
+public class No {
+    String nome;
+    No proximo;
+
+    public No(String nome) {
+        this.nome = nome;
+        this.proximo = null;
+    }
+
+    // Getters e setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+
+    }
+}
