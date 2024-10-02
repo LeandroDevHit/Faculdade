5f6bf0e318018092061bb0b3c040c1b960530227
LoneHitori <leandrohitorii@gmail.com>
2024-10-02 14:36:37 -0300

    Update Main.java

--- Estrutura de Dados/ListaEncadeadaSimples/Main.java
+++ Estrutura de Dados/ListaEncadeadaSimples/Main.java
@@ -1,1 +1,16 @@
+package listencadeada;
 
+public class Main {
+    public static void main(String[] args) {
+        ListaEncadeada lista = new ListaEncadeada();
+        lista.inserirNoInicio("Leandro");
+        lista.inserirNoFim("Roger");
+        lista.inserirNoInicio("Clodovaldo");
+        lista.inserirNoInicio("João");
+        lista.imprimir();
+        lista.remover("João");
+        lista.imprimir();
+        System.out.println(lista.buscar("João"));
+        System.out.println(lista.buscar("Leandro"));
+    }
+}
